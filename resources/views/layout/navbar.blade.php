8be02276f35e19c1024deaff11b56bf51975e12f
Duck <40841131@gm.nfu.edu.tw>
2021-12-21 23:29:19 +0800

    改navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="{{route('webtest')}}">Test1</a>
+    <a class="navbar-brand" href="{{route('webtest')}}">記帳本</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="{{route('test2')}}">Test2</a>
+                <a class="nav-link" href="{{route('test2')}}">新增</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
