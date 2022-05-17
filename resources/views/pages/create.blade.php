37df4dbbe752a67152dca814c879309f4c62342a
Duck <40841131@gm.nfu.edu.tw>
2022-05-17 21:16:44 +0800

    改新增的畫面

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -3,46 +3,21 @@
     <div class="container">
         <form class="form-group" action="{{route('get_create_data')}}" method="post">
             @csrf
-            <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-secondary active">
-                    <input type="radio" name="type" autocomplete="off" checked value="收入"> 收入
-                </label>
-                <label class="btn btn-secondary">
-                    <input type="radio" name="type" autocomplete="off" value="支出"> 支出
-                </label>
-            </div>
-{{--            <div class="form-group">--}}
-{{--                <select class="form-control form-control-lg selectpicker" id="FormControlSelect1" name="type">--}}
-{{--                    <option>收入</option>--}}
-{{--                    <option>支出</option>--}}
-{{--                </select>--}}
-{{--            </div>--}}
             <div class="form-group">
-                <label class="h1">日期</label>
-                <script>
-                    $(function() {
-                        $( "#datepicker" ).datepicker();
-                    });
-                </script>
-                <input class="form-control col-sm-15" name="date"  id="datepicker">
-            </div>
-            <div class="form-group">
-                <label class="h1">類別</label>
-                <select class="form-control form-control-lg selectpicker" id="FormControlSelect1" name="item">
-                    <option name="food">食物</option>
-                    <option name="necessary">日用品</option>
-                    <option name="traffic">交通</option>
-                    <option name="game">娛樂</option>
-                    <option name="life">生活繳費</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label class="h1">名稱</label>
+                <label class="h1">姓名</label>
                 <input class="form-control col-sm-15" name="name" >
             </div>
             <div class="form-group">
-                <label class="h1">價錢</label>
-                <input class="form-control col-sm-15" name="price">
+                <label class="h1">電話</label>
+                <input class="form-control col-sm-15" name="phone">
+            </div>
+            <div class="form-group">
+                <label class="h1">地址</label>
+                <input class="form-control col-sm-15" name="address">
+            </div>
+            <div class="form-group">
+                <label class="h1">時段</label>
+                <input class="form-control col-sm-15" name="time">
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1" class="h1">備註</label>
@@ -53,16 +28,5 @@
             </div>
         </form>
     </div>
-
-    <script>
-        $(function() {
-            $( "#datepicker" ).datepicker();
-        });
-    </script>
-{{--    <script>--}}
-{{--        function in() {--}}
-{{--            type = --}}
-{{--        }--}}
-{{--    </script>--}}
 
 @endsection
