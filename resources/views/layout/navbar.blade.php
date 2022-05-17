67f61eb04d3ece3a5090937c3efdc4027309045f
Duck <40841131@gm.nfu.edu.tw>
2022-05-17 21:52:17 +0800

    navbar小修

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,17 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="{{route('index')}}">JunkBaby</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{route('index')}}">JunkBaby</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('create')}}">新增</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('chart')}}">圖表</a>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('create')}}">新增</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('chart')}}">圖表</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
